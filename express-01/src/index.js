fe56a576b5be26b9b915c849ceafa15c6ae78cf2
copy <Rafael.deassis@yahoo.com.br>
2026-09-11 09:49:40 -0300

    Neon

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,41 +1,28 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import models from "./models/index.js";
-import routes from './routes/index.js';
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import models, { sequelize } from "./models/index.js";
+import routes from "./routes/index.js";
 
 const app = express();
 
-app.set('trust proxy', true); 
+app.set("trust proxy", true);
 
 // middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// middleware the logs
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next();
-});
-// middleware de autenticação "fake" + injeção dos models no req.context
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
-  next();
-});
 
 // rotas
-app.get('/', (req, res) => {
-  return res.send('Servidor express exectuando...');
+app.get("/", (req, res) => {
+  return res.send("Servidor express exectuando...");
 });
-app.use('/session', routes.session);
-app.use('/users', routes.user);
-app.use('/messages', routes.message);
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
-);
+const eraseDatabaseOnSync = process.env.ERASE_DATABASE_ON_SYNC === "true";
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
