e5e66600c22a1da258c392d4ba03de74190cd7e5
copy <Rafael.deassis@yahoo.com.br>
2026-09-15 10:53:28 -0300

    limpesa

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -10,6 +10,21 @@ app.set("trust proxy", true);
 
 // middlewares
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// middleware the logs
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+// middleware de autenticação "fake" + injeção dos models no req.context
+app.use(async (req, res, next) => {
+  req.context = {
+    models,
+    me: await models.User.findByLogin("rwieruch"),
+  };
+  next();
+});
 
 // rotas
 app.get("/", (req, res) => {
@@ -24,5 +39,43 @@ const port = process.env.PORT || 3000;
 const eraseDatabaseOnSync = process.env.ERASE_DATABASE_ON_SYNC === "true";
 
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: "rwieruch",
+      email: "rwieruch@email.com",
+      messages: [
+        {
+          text: "Published the Road to learn React",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: "ddavids",
+      email: "ddavids@email.com",
+      messages: [
+        {
+          text: "Happy to release ...",
+        },
+        {
+          text: "Published a complete ...",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
